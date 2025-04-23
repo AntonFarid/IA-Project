b11619dd07b6a825f11071433838c6f30e148a85
AntonFarid <tonyfarid34@gmail.com>
2025-04-24 00:53:47 +0200

    starting project

--- obj/Debug/net8.0/IA-Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/IA-Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IA-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f869dccd02c817063d83d85da90a418b977cd005")]
 [assembly: System.Reflection.AssemblyProductAttribute("IA-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IA-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
